07fc1eb253e4a7e1586bf0a4dd64ccf5513f40c2
Unknown <158840284@qq.com>
2018-11-13 22:43:54 -0600

    generate post request and navigation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions";
 
 class PostsNew extends Component {
   renderField(field) {
@@ -18,7 +21,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push("/");
+    });
   }
 
   render() {
@@ -44,6 +49,9 @@ class PostsNew extends Component {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
@@ -69,4 +77,9 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: "PostsNewForm"
-})(PostsNew);
+})(
+  connect(
+    null,
+    { createPost }
+  )(PostsNew)
+);
